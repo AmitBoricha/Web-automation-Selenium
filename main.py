53dd46c5603e5663a4767c42db48ae0fb5623714
AmitBoricha <borichaamit1@gmail.com>
2023-09-30 23:14:19 +0530

    added

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # This is a sample Python script.
-from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -18,10 +14,3 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-def open_browser():
-    driver = webdriver.Chrome()
-    return driver
-
-def close_browser(driver):
-    driver.quit()
