741f26b877fa53041360ef3a270141cb793d35f4
AmitBoricha <borichaamit1@gmail.com>
2023-09-30 23:14:39 +0530

    added

--- alert_and_popup_handling.py
+++ alert_and_popup_handling.py
@@ -4,3 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def open_browser():
+    driver = webdriver.Chrome()
+    return driver
