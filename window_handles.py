2723ce4a01a739f1fcd5de01f3e2fc051315fda9
AmitBoricha <borichaamit1@gmail.com>
2023-09-30 23:07:54 +0530

    added

--- window_handles.py
+++ window_handles.py
@@ -9,3 +9,10 @@ driver.get("https://www.google.com")
 
 # Store the handle of the main window
 main_window_handle = driver.window_handles[0]
+
+# After opening the new window, the focus will shift to it automatically
+driver.execute_script("window.open('https://www.google.com', '_blank');")
+
+# Switch to the newly opened window
+new_window_handle = driver.window_handles[-1]
+driver.switch_to.window(new_window_handle)
