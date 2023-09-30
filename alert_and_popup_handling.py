a8d0f3b2e6cb546af733e407ca0c4303682347e3
AmitBoricha <borichaamit1@gmail.com>
2023-09-30 23:13:16 +0530

    added

--- alert_and_popup_handling.py
+++ alert_and_popup_handling.py
@@ -3,3 +3,4 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # This is a sample Python script.
-
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,3 +18,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def open_browser():
+    driver = webdriver.Chrome()
+    return driver
