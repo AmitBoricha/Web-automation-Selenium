0c06410c7bb83494f6b4e2d3f7dfdb1571a2dfd0
AmitBoricha <borichaamit1@gmail.com>
2023-10-03 00:10:46 +0530

    added

--- form_elements_handling.py
+++ form_elements_handling.py
@@ -7,3 +7,6 @@ driver.get("https://www.example.com")
 text_box = driver.find_element_by_id("username")
 text_box.clear()
 text_box.send_keys("my_username")
+
+# Find and interact with a button element
+submit_button = driver.find_element_by_id("submit_button")
