1073147184cc7f33075a5f11301039def3dc3fc5
AmitBoricha <borichaamit1@gmail.com>
2023-10-02 23:50:28 +0530

    added

--- handling_web_tables.py
+++ handling_web_tables.py
@@ -2,3 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+def open_browser():
+    driver = webdriver.Chrome()
+    return driver
+
