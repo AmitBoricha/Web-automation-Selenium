66d64b6a9cd76eaf9a87e8628e6ba1cefdaebf8e
AmitBoricha <borichaamit1@gmail.com>
2023-10-02 23:51:19 +0530

    added

--- handling_web_tables.py
+++ handling_web_tables.py
@@ -9,3 +9,15 @@ def open_browser():
 
 def close_browser(driver):
     driver.quit()
+
+def get_table_data(driver, table_id):
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, table_id)))
+        table = driver.find_element(By.ID, table_id)
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        table_data = []
+
+        for row in rows:
+            columns = row.find_elements(By.TAG_NAME, "td")
+            row_data = [column.text for column in columns]
+            table_data.append(row_data)
