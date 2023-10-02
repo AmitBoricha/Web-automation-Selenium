406e43f95ef7648b366c9dd468f2745d9bdc8a31
AmitBoricha <borichaamit1@gmail.com>
2023-10-03 00:10:34 +0530

    added

--- form_elements_handling.py
+++ form_elements_handling.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 driver.get("https://www.example.com")
+
+text_box = driver.find_element_by_id("username")
+text_box.clear()
+text_box.send_keys("my_username")
