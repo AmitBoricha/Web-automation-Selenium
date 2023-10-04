cc0d2c6b4a8e6274569922eb5bc58a3a4e582b25
AmitBoricha <borichaamit1@gmail.com>
2023-10-04 22:46:18 +0530

    added

--- checkbox_selection.py
+++ checkbox_selection.py
@@ -4,3 +4,7 @@ driver = webdriver.Chrome()
 
 driver.get("https://www.example.com")
 
+# Locate and select a checkbox element
+checkbox = driver.find_element_by_id("checkbox_id")  # Replace with the actual element locator
+if not checkbox.is_selected():
+    checkbox.click()  # Select the checkbox
