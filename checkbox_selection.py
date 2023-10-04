aba03bdfae6cde88a1ff00f69e058c0fd4d6f98f
AmitBoricha <borichaamit1@gmail.com>
2023-10-04 22:47:08 +0530

    added

--- checkbox_selection.py
+++ checkbox_selection.py
@@ -9,6 +9,10 @@ checkbox = driver.find_element_by_id("checkbox_id")  # Replace with the actual e
 if not checkbox.is_selected():
     checkbox.click()  # Select the checkbox
 
+radio_button = driver.find_element_by_id("radio_button_id")  # Replace with the actual element locator
+if not radio_button.is_selected():
+    radio_button.click()  # Select the radio button
+
 
 # Close the browser
 driver.quit()
