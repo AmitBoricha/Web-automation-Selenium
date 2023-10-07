6ce85caeb57711f498708da2414c841144f45890
AmitBoricha <borichaamit1@gmail.com>
2023-10-07 23:12:37 +0530

    added

--- mouse_click_event.py
+++ mouse_click_event.py
@@ -4,3 +4,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # Initialize the WebDriver
 driver = webdriver.Chrome()
+
+driver.get("https://www.example.com")
+
+# Locate the element to click
+element_to_click = driver.find_element_by_id("element_id")
+
+# Create an ActionChains object
+action = ActionChains(driver)
