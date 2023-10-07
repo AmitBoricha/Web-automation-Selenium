44d05daa7d4c5980bb039053fb55d18b18b8cab8
AmitBoricha <borichaamit1@gmail.com>
2023-10-07 23:13:50 +0530

    added

---
+++ keyboard_events.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+
+# Open a web page
+driver.get("https://www.example.com")
