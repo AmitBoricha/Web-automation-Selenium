1fd0ef24c0b38546033bdf66722720c5ecdb3cfe
AmitBoricha <borichaamit1@gmail.com>
2023-10-31 22:52:59 +0530

    added'

--- github_login.py
+++ github_login.py
@@ -4,3 +4,6 @@ from selenium.webdriver.common.keys import Keys
 
 
 class TestGithubPage(BaseClass):
+    def test_1_login_wrong_username_wrong_password(self):
+        self.log().info("Github Login Page Bad case Test Started")
+        self.driver.get("https://github.com/login")
